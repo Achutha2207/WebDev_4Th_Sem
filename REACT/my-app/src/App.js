a8bb96bdb1f7bf3cc1645bc5af609e77522251a0
Achutha2207 <achutha22072004@gmail.com>
2024-09-05 10:33:34 +0530

    updated

---
+++ REACT/my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+let name = "Achutha";
+
+function App() {
+  return (
+    <>
+      <h1>Hello {name}</h1>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+      <div className="conatiner">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum iure
+          nostrum ducimus aspernatur sed fugiat temporibus hic quaerat
+          reprehenderit omnis dolores, ex ipsam consequuntur commodi dolorum.
+          Dolorum, quod nisi aspernatur libero consectetur atque obcaecati quia
+          nemo rem illo, repudiandae excepturi unde iusto cupiditate porro fuga
+          laborum, veritatis possimus dolores deserunt? Fuga molestias dolorem
+          similique? A architecto, ducimus animi dicta ut similique quis nulla
+          delectus numquam fuga quasi ex nihil, facilis molestiae facere, veniam
+          voluptate assumenda deleniti! Ipsum libero eius soluta est esse nisi
+          accusamus adipisci assumenda omnis ex, voluptas sequi placeat beatae.
+          Temporibus reprehenderit sequi voluptates consectetur ipsum totam
+          optio officia, accusantium laborum minima explicabo impedit rerum
+          delectus cumque at, cupiditate exercitationem aspernatur quae! Beatae
+          doloremque quia excepturi consequuntur deserunt accusamus, animi
+          molestiae corporis, pariatur eius fuga rem at veniam tempore nemo
+          voluptatem aliquid. Excepturi ratione sunt beatae molestiae a harum
+          ipsum eaque. Ipsum, perspiciatis eius dolores deleniti fugit nihil
+          quas perferendis sunt laudantium excepturi officiis nostrum
+          doloremque, a, quibusdam voluptas velit. Molestias nam dolores at
+          tempora, provident incidunt repellat et ullam! Ducimus itaque ut esse
+          error harum odio animi architecto quo repudiandae adipisci excepturi
+          quibusdam, consequuntur voluptatibus, ab deserunt beatae. Magnam dolor
+          rerum quae, facere non quod. Eveniet, dolores!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
